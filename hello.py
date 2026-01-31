859fbdbeee083846939d3b0c09f6541a744a9dd6
Leonardo Zeaiter <leonardozeaiter@gmail.com>
2026-01-31 13:08:17 +0200

    initial commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+import math
+class Person:
+    def __init__(self, id: int, name: str) -> None:
+        self.id = id
+        self.name = name
+
+    def __repr__(self):
+        return f"Person(id={self.id}, name={self.name})"
+    
+    def add(self, a: int, b: int) -> int:
+        """
+        Adds two integers and return the sum as integer
+        
+        :param self: the entity
+        :param a: first integer
+        :type a: int
+        :param b: second integer
+        :type b: int
+        :return: Returns the sum of a and b
+        :rtype: int
+        """
+        return a + b
+    
+
+leo = Person(1, "Leonardo Zeaiter")
+print(dir(leo))
+leo.add(1, 2)
+help(leo.add)
